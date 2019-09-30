b59c8a3dcfded482bfc1e5bca3165fe00f0b8ec5
mingjun <sickcats@163.com>
2019-09-30 10:20:59 +0800

    'delete_file'

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from '@/utils/axios'
 
 export const getBannerList = async (type = 2) => {
-    return await axios.get(`/banner?type=${type}`);
+    return await axios.get(`/banner?type=${type}`)
 }
 
